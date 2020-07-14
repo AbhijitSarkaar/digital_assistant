14ecaa7b525aff2b49a0d8c91ea25a3363526f5a
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-07-14 17:20:44 +0530

    Add Header, ChatWindow and Input components

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './Header';
+import ChatWindow from './ChatWindow';
+import Input from './Input';
 import styles from './App.module.css';
 
 function App() {
   return (
     <div className={styles.App}>
-      
+      <Header />
+      <ChatWindow />
+      <Input />
     </div>
   );
 }
---
+++ src/modules/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Header = () => {
+    return (
+        <div className={styles.headerContainer}>
+            
+        </div>
+    );
+};
+
+export default Header;
