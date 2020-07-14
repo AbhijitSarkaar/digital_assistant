9e56e91735707d4edcad534f2da65b18f450d46c
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-07-14 18:57:44 +0530

    Header completed

--- src/modules/Header/index.js
+++ src/modules/Header/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
 import styles from './index.module.css';
 
 const Header = () => {
     return (
         <div className={styles.headerContainer}>
-            
+            <MenuRoundedIcon classes={{'root': styles.menuIcon}} />
+            <div className={styles.headerTextContainer}>
+                <p className={styles.headerText}>
+                    Digital Assistant
+                </p>
+            </div>
         </div>
     );
 };
--- src/modules/Input/index.js
+++ src/modules/Input/index.js
@@ -4,7 +4,6 @@ import styles from './index.module.css';
 const Input = () => {
     return (
         <div className={styles.inputContainer}>
-            
         </div>
     );
 };
